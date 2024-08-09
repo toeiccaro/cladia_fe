97ec94ada0306d2035cf7fb5e9feb6c5b7f88447
HieuNQ-DxTech <hieu.ngo@dxtech.jp>
2024-08-09 13:05:15 +0700

    Fix bug: - Setting finance : fix label - Receive browse

--- components/MasterData/Parameter/TableParameter.vue
+++ components/MasterData/Parameter/TableParameter.vue
@@ -207,8 +207,7 @@ export default {
         const headerItem = {
           key: item.fieldName,
           filter: "input",
-          // name: this.$t(`lan_${item.fieldName}_0`),
-          name: this.$t(`lbl_${item.fieldName}_0`),
+          name: this.shouldShowCodeField ? this.$t(`lbl_${item.fieldName}_0`) : this.$t(`lan_${item.fieldName}_0`),
           width: 150,
           fieldName: item.fieldName,
           fieldOrder: index,
