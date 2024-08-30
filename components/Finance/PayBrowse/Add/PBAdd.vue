d375e9146d8c31bc3a007452317521610cd158d0
HieuNQ-DxTech <hieu.ngo@dxtech.jp>
2024-08-30 10:46:48 +0700

    Fix bug sprints

--- components/Finance/PayBrowse/Add/PBAdd.vue
+++ components/Finance/PayBrowse/Add/PBAdd.vue
@@ -735,7 +735,7 @@ export default {
           const response = await api('addAPPB', params)
           const errorCode = response?.data?.response?.status
 
-          if (errorCode === SERVER_RESPONSE_CODE.FOPBIDDEN) {
+          if (errorCode === SERVER_RESPONSE_CODE.FORBIDDEN) {
             window.alert(this.$t(response?.data?.response?.data?.message))
             return
           }
